e2e86fdac81b66a85750920d588ce77492e1048f
Jose Martinez <jmmartinez.desarrollo@gmail.com>
2023-07-16 14:11:27 +0200

    Add cache items

--- src/components/ECIFillText.js
+++ src/components/ECIFillText.js
@@ -1,76 +1,84 @@
 
-  let itemsList = [{ id: Math.floor(Math.random() * 1000), value: 'item 1' }, { id: Math.floor(Math.random() * 1000), value: 'item 2' }, { id: Math.floor(Math.random() * 1000), value: 'item 3' }]
-	let items = document.getElementsByName('item')
-	const ul = document.getElementById('listitemsList')
-	const inputNewValue = document.getElementById("input-newValue")
-	const buttonAdd = document.getElementById('addItem')
-	const buttonDelete = document.getElementById('deleteItem')
-	const openModal = document.getElementsByName('openModal')
+    let items = [{ id: Math.floor(Math.random() * 1000), value: 'item 1' }, { id: Math.floor(Math.random() * 1000), value: 'item 2' }, { id: Math.floor(Math.random() * 1000), value: 'item 3' }]
+	
+	let HTMLitems = document.getElementsByName('item')
+	const HTMLitemsList = document.getElementById('itemsList')
+	const HTMLbadgeCached = document.getElementById("badgeCached")
+
+	const inputValue = document.getElementById("input-addValue")
+	const buttonAdd = document.getElementById('button-addItem')
+	const buttonDelete = document.getElementById('button-deleteItem')
+	const buttonCached = document.getElementById('button-cachedItem')
+	
 	let ECImodal = document.getElementsByName('modal')
-	let memory = []
-	let selectedItems = []
+	const openModal = document.getElementsByName('openModal')
 
+	let itemSelected = []
+	let itemsCached = []
+	
+		
+	//Function to show items in HTML and create Event Lisener to click items
+	let showItems = ()=>{
+		items.map((item) => {
+			let li = document.createElement('li');
+			li.setAttribute('id', item.id)
+			li.setAttribute('name', 'item')
+			li.textContent = item.value
+			HTMLitemsList.append(li)
+		}) 		
+		HTMLitems.forEach((item) => {
+			item.addEventListener('click', function () {
+				let HTMLitems = document.getElementsByName('item')
+				let isSelected = item.hasAttribute('selected');
 
-	//Show default items
-	itemsList.map((item) => {
-		let li = document.createElement('li');
-		li.setAttribute('id', item.id)
-		li.setAttribute('name', 'item')
-		li.textContent = item.value
-		ul.append(li)
-	})
-  //Event Lisener click default items
-	items.forEach((item) => {
-		item.addEventListener('click', function () {
-			let items = document.getElementsByName('item')
-			let isSelected = item.hasAttribute('selected');
+				let itemId = item.getAttribute('id');
+				if (isSelected) {			
+					item.removeAttribute('selected', false)
+					itemSelected.splice(itemSelected.includes(parseInt(itemId)), 1)
 
-			let itemId = item.getAttribute('id');
-			if (isSelected) {			
-				item.removeAttribute('selected', false)
-				selectedItems.splice(selectedItems.includes(parseInt(itemId)), 1)
-
-			} else {			
-				item.setAttribute('selected', true)
-				selectedItems.push(itemId)
-			}
-			let isAllSelected = Array.from(items.values()).every((_item) => {
-				if (_item.hasAttribute('selected')) {
-					return true;
-
+				} else {			
+					item.setAttribute('selected', true)
+					itemSelected.push(itemId)
 				}
+				let isAllSelected = Array.from(HTMLitems.values()).every((_item) => {
+					if (_item.hasAttribute('selected')) {
+						return true;
+
+					}
+				})		
+			//	isAllSelected ? buttonDelete.setAttribute("disabled", true) : buttonDelete.removeAttribute("disabled")
 			})
-		//	isAllSelected ? buttonDelete.setAttribute("disabled", true) : buttonDelete.removeAttribute("disabled")
 		})
-	})
+	}
   
-	//Button add item
+	//Event button add items
 	buttonAdd.addEventListener('click', function () {
 
-		//buttonAdd.setAttribute("disabled", true)
-		let li = document.createElement('li');
+		//buttonAdd.setAttribute("disabled", true)		
 	
-			let hasValue = inputNewValue.value != '';
+			let hasValue = inputValue.value != '';
       
 			if (hasValue) {
-				let newId = Math.floor(Math.random() * 1000)
-				itemsList.push({ id: newId, value: inputNewValue.value })
-				memory.push({ id: newId, name: inputNewValue.value })
+				itemsCached.push([...items])	
+				let li = document.createElement('li');
+				let newId = Math.floor(Math.random() * 1000)				
+				items.push({ id: newId, value: inputValue.value })						
 				li.setAttribute('id', newId)
 				li.setAttribute('name', 'item')
-				li.textContent = inputNewValue.value				
-				inputNewValue.value = ''
+				li.textContent = inputValue.value				
+				inputValue.value = ''
+				HTMLbadgeCached.textContent = itemsCached.length
         li.addEventListener('click', function () {
           let isSelected = li.hasAttribute('selected');
           if (isSelected) {        
             li.removeAttribute('selected', false)
-            selectedItems.splice(selectedItems.includes(parseInt(itemId)), 1)
+            itemSelected.splice(itemSelected.includes(parseInt(itemId)), 1)
     
           } else {          
             li.setAttribute('selected', true)
-            selectedItems.push(newId)
+            itemSelected.push(newId)
           }
-          			let isAllSelected = Array.from(items.values()).every((_item) => {
+          			let isAllSelected = Array.from(HTMLitems.values()).every((_item) => {
 				if (_item.hasAttribute('selected')) {
 					return true;
 
@@ -78,19 +86,27 @@
 			})
 		//	isAllSelected ? buttonDelete.setAttribute("disabled", true) : buttonDelete.removeAttribute("disabled")
         })
-        ul.append(li)
+        HTMLitemsList.append(li)
 			}
 			ECImodal.forEach((_modal)=>{
 				_modal.classList.remove("active")
 			})		
-		})
+	})
 
-
-	//Button remove items event
+	//Event button remove items 
 	buttonDelete.addEventListener('click', function () {
-		let items = document.getElementsByName('item')
+		let HTMLitems = document.getElementsByName('item')
+		itemsCached.push([...items])	
+		
+		HTMLitems.forEach((item) => {
+			itemSelected.forEach((selected) => {
+				if (parseInt(item.id) == selected) {
+					item.remove()
+				}
+			})
+		})
 		items.forEach((item) => {
-			selectedItems.forEach((selected) => {
+			itemSelected.forEach((selected) => {
 				if (parseInt(item.id) == selected) {
 					item.remove()
 				}
@@ -99,10 +115,21 @@
 
 
 	})
- 
 
-		//Button open modal
-		openModal.forEach((modal)=>{
+	//Even button cached items
+	buttonCached.addEventListener('click', function(){		
+		items = [...itemsCached[itemsCached.length -1]]
+		itemsCached.pop()	
+		let firstItem = HTMLitemsList.firstElementChild
+		while(firstItem ){
+			firstItem.remove()
+			firstItem = HTMLitemsList.firstElementChild
+		}
+		showItems()
+	})
+
+	//Button open/close modal
+	openModal.forEach((modal)=>{
 			modal.addEventListener('click', function () {					
 				let isActive = ECImodal[0].classList.contains("active");
 				if(isActive){
@@ -116,5 +143,8 @@
 				}
 			
 			})
-		})
+	})
 	
+
+	// Add item to fist time
+	showItems()
